916e9d209c7ddbfef0bf593567161108b65cf811
PRIZEOX <oganisyanararat1@gmail.com>
2023-08-05 00:17:11 +0400

    card page ui finished

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICart, ICartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl:'./cart.component.html',
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  cart:ICart = {items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'foot',
+    price: 350,
+    quantity: 2,
+    id: 1,
+  }
+]};
+
+  dataSource: Array<ICartItem> = [];
+  displayedColumns : Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items:Array<ICartItem>):number{
+    return items.
+    map((item) => item.price * item.quantity).
+    reduce((prev,curr) => prev +curr, 0);
+  }
 
 }
